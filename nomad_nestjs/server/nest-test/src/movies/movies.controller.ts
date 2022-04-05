53701ef79d63cfec94110984fd34bdec2c01459c
Kwon <anstjdrnjs3@gmail.com>
2022-04-05 14:50:02 +0900

    transform 옵션으로 인한 id 타입 재지정, DTO 타입 지정

--- nomad_nestjs/server/nest-test/src/movies/movies.controller.ts
+++ nomad_nestjs/server/nest-test/src/movies/movies.controller.ts
@@ -1,4 +1,6 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post} from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 // @Controller('~~') 이 부분이 url의 entry point 컨트롤
@@ -12,27 +14,22 @@ export class MoviesController {
     }
 
     @Get("/:id")
-    getOne(@Param('id') movieId: string ): Movie {
+    getOne(@Param('id') movieId: number ): Movie {
         return this.moviesService.getOne(movieId);
     }
 
     @Post()
-    create(@Body() movieData) {
+    create(@Body() movieData: CreateMovieDto) {
         return this.moviesService.create(movieData);
     }
 
     @Delete("/:id")
-    remove(@Param('id') movieId: string) {
+    remove(@Param('id') movieId: number) {
         return this.moviesService.deleteOne(movieId);
     }
 
     @Patch('/:id')
-    path(@Param('id') movieId: string, @Body() updateData) {
-        return {
-            updatedMovie: movieId,
-            ...updateData
-        }
+    path(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
+        return this.moviesService.update(movieId, updateData)
     }
-
-    
 }
