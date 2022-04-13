19a694a1e3d2d11c74e73ae182c12d65756ca317
Kwon <anstjdrnjs3@gmail.com>
2022-04-13 22:08:47 +0900

    learn middleware

--- NestJS/letsStart/dist/app.js
+++ NestJS/letsStart/dist/app.js
@@ -3,9 +3,23 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var app_model_1 = require("./app.model");
 var app = express();
+app.use(function (req, res, next) {
+    console.log(req.rawHeaders[1]);
+    next();
+});
+app.get("/cat/0", function (req, res, next) {
+    console.log("This is cats id: 0 middleware");
+    next();
+});
 app.get("/", function (req, res) {
-    console.log(req);
     res.send({ cats: app_model_1.Cat });
+});
+app.get("/cat/0", function (req, res) {
+    res.send({ blue: app_model_1.Cat[0] });
+});
+app.use(function (req, res) {
+    console.log("this is error middleware");
+    res.send({ error: "404 Not Found error" });
 });
 app.listen(3000, function () {
     console.log("server is on ...");
--- NestJS/letsStart/src/app.ts
+++ NestJS/letsStart/src/app.ts
@@ -1,11 +1,30 @@
 import * as express from "express";
+import { nextTick } from "process";
 import { Cat, CatType } from "./app.model";
 
 const app: express.Express = express();
 
+app.use((req, res, next) => {
+  console.log(req.rawHeaders[1]);
+  next();
+});
+
+app.get("/cat/0", (req, res, next) => {
+  console.log("This is cats id: 0 middleware");
+  next();
+});
+
 app.get("/", (req: express.Request, res: express.Response) => {
-  console.log(req);
   res.send({ cats: Cat });
+});
+
+app.get("/cat/0", (req: express.Request, res: express.Response) => {
+  res.send({ blue: Cat[0] });
+});
+
+app.use((req, res) => {
+  console.log("this is error middleware");
+  res.send({ error: "404 Not Found error" });
 });
 
 app.listen(3000, () => {
