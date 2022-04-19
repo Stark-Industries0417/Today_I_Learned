46317ed7a017d6b8e0c61cb6390510c95472cd48
Kwon <anstjdrnjs3@gmail.com>
2022-04-19 22:59:57 +0900

    validation

--- NestJS/firstProject/src/main.ts
+++ NestJS/firstProject/src/main.ts
@@ -1,9 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT;
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(PORT);
 }
 bootstrap();
