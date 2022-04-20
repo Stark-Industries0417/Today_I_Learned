40abd12c67def08f0c12ef19018027908444f212
Kwon <anstjdrnjs3@gmail.com>
2022-04-20 17:35:41 +0900

    nest/swagger setting

--- NestJS/firstProject/src/main.ts
+++ NestJS/firstProject/src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 
@@ -8,6 +9,16 @@ async function bootstrap() {
   const PORT = process.env.PORT;
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('C.I.C')
+    .setDescription('cat')
+    .setVersion('1.0.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(PORT);
 }
 bootstrap();
