4bae7253a0d9c826fd9fd9c14c2dbea66f9275fd
Kwon <anstjdrnjs3@gmail.com>
2022-04-20 17:36:34 +0900

    스웨거 상속으로 재사용성

---
+++ NestJS/firstProject/src/cats/dto/cat.dto.ts
@@ -0,0 +1,10 @@
+import { ApiProperty, PickType } from '@nestjs/swagger';
+import { Cat } from '../cats.schema';
+
+export class ReadOnlyCatDto extends PickType(Cat, ['email', 'name'] as const) {
+  @ApiProperty({
+    example: '328012',
+    description: 'id',
+  })
+  id: string;
+}
