9e1513a97c9a5768e3d0b65dec3d589a3b08ab78
Kwon <anstjdrnjs3@gmail.com>
2022-04-23 21:03:04 +0900

    settings default image in schema

--- NestJS/firstProject/src/cats/cats.schema.ts
+++ NestJS/firstProject/src/cats/cats.schema.ts
@@ -51,11 +51,19 @@ export class Cat extends Document {
     description: 'img url',
     required: true,
   })
-  @Prop()
+  @Prop({
+    default:
+      'https://github.com/amamov/NestJS-solid-restapi-boilerplate/raw/main/docs/images/1.jpeg',
+  })
   @IsString()
   imgUrl: string;
 
-  readonly readOnlyData: { id: string; email: string; name: string };
+  readonly readOnlyData: {
+    id: string;
+    email: string;
+    name: string;
+    imgUrl: string;
+  };
 }
 
 export const CatSchema = SchemaFactory.createForClass(Cat);
@@ -65,5 +73,6 @@ CatSchema.virtual('readOnlyData').get(function (this: Cat) {
     id: this.id,
     email: this.email,
     name: this.name,
+    imgUrl: this.imgUrl,
   };
 });
