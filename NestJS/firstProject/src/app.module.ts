9eb97fd727e292f01901277c82db386243ef1b54
Kwon <anstjdrnjs3@gmail.com>
2022-04-27 20:10:17 +0900

    setting mongoose

--- NestJS/firstProject/src/app.module.ts
+++ NestJS/firstProject/src/app.module.ts
@@ -1,7 +1,7 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import mongoose from 'mongoose';
+import * as mongoose from 'mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
@@ -12,7 +12,9 @@ import { CommentsModule } from './comments/comments.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGODB_URI),
+    MongooseModule.forRoot(process.env.MONGODB_URI, {
+      useCreateIndex: true,
+    }),
     CatsModule,
     AuthModule,
     CommentsModule,
