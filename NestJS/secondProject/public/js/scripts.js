9cb76ea40de5e9197a785a8eec54e4b2eeb297e5
Kwon <anstjdrnjs3@gmail.com>
2022-04-30 21:19:23 +0900

    user socket

--- NestJS/secondProject/public/js/scripts.js
+++ NestJS/secondProject/public/js/scripts.js
@@ -1,4 +1,4 @@
-const socket = io('/');
+const socket = io('/chattings');
 
 const getElementById = (id) => document.getElementById(id) || null;
 
@@ -6,16 +6,19 @@ const header = getElementById('hello_stranger');
 const chattingBox = getElementById('chatting_box');
 const formElement = getElementById('chat_form');
 
+socket.on('connected_user', (username) => {
+  console.log(username);
+});
+
 function helloUser() {
   const userName = prompt('What is your name?');
-  // 리턴값으로 응답 받기
-  socket.emit('new_user', userName, (data) => {
-    console.log(data);
-  });
 
-  // 서버의 socket.emit 의 값 받기
-  socket.on('hello_user', (data) => {
-    console.log(data);
+  const drawHelloStranger = (username) => {
+    header.innerText = `Hello ${username} Stranger`;
+  };
+  // 리턴값으로 응답 받기
+  socket.emit('new_user', userName, (username) => {
+    drawHelloStranger(username);
   });
 }
 
