2a2e146c4a14db268fa2d260b554994d64b7467c
Kwon <anstjdrnjs3@gmail.com>
2022-04-30 21:19:33 +0900

    scoket server example

--- NestJS/secondProject/src/chats/chats.gateway.ts
+++ NestJS/secondProject/src/chats/chats.gateway.ts
@@ -1,21 +1,39 @@
+import { Logger } from '@nestjs/common';
 import {
   ConnectedSocket,
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
 } from '@nestjs/websockets';
-import { Socket } from 'dgram';
+import { Socket } from 'socket.io';
 
-@WebSocketGateway()
-export class ChatsGateway {
+@WebSocketGateway({ namespace: 'chattings' })
+export class ChatsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private logger = new Logger('chat');
+  // 소켓 게이트웨이 실행되었을 때 가장 먼저 실행되는 메서드
+  afterInit() {
+    this.logger.log('init');
+  }
+  // 클라이언트와 연결되자마자 실행되는 메서드
+  handleConnection(@ConnectedSocket() socket: Socket) {
+    this.logger.log(`connected: ${socket.id} ${socket.nsp.name}`);
+  }
+
+  handleDisconnect(@ConnectedSocket() socket: Socket) {
+    this.logger.log(`disconnected: ${socket.id} ${socket.nsp.name}`);
+  }
+
   @SubscribeMessage('new_user')
   handleNewUser(
     @MessageBody() userName: string,
     @ConnectedSocket() socket: Socket,
   ) {
-    console.log(userName);
-
-    socket.emit('hello_user', `hello ${userName}`);
-    return `hello ${userName}`;
+    socket.broadcast.emit('connected_user', userName);
+    return userName;
   }
 }
