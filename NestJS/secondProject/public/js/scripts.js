03df8c087d5d5403363e188051504fefd66b6a2e
Kwon <anstjdrnjs3@gmail.com>
2022-04-30 20:36:16 +0900

    practice socket

--- NestJS/secondProject/public/js/scripts.js
+++ NestJS/secondProject/public/js/scripts.js
@@ -1,3 +1,26 @@
-const h1 = document.querySelector('h1');
+const socket = io('/');
 
-h1.innerText = 'aaa';
+const getElementById = (id) => document.getElementById(id) || null;
+
+const header = getElementById('hello_stranger');
+const chattingBox = getElementById('chatting_box');
+const formElement = getElementById('chat_form');
+
+function helloUser() {
+  const userName = prompt('What is your name?');
+  // 리턴값으로 응답 받기
+  socket.emit('new_user', userName, (data) => {
+    console.log(data);
+  });
+
+  // 서버의 socket.emit 의 값 받기
+  socket.on('hello_user', (data) => {
+    console.log(data);
+  });
+}
+
+function init() {
+  helloUser();
+}
+
+init();
