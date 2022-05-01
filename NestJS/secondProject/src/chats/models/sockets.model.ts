a112cceb02330f39e3c7cc3eebc6f2691ffddc73
Kwon <anstjdrnjs3@gmail.com>
2022-05-01 23:29:06 +0900

    socket schema

---
+++ NestJS/secondProject/src/chats/models/sockets.model.ts
@@ -0,0 +1,29 @@
+import { Prop, Schema, SchemaFactory, SchemaOptions } from '@nestjs/mongoose';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { Document } from 'mongoose';
+
+const options: SchemaOptions = {
+  id: false,
+  collection: 'chattings',
+  timestamps: true,
+};
+
+@Schema(options)
+export class SocketModel extends Document {
+  @Prop({
+    unique: true,
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsString()
+  id: string;
+
+  @Prop({
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsString()
+  username: string;
+}
+
+export const SocketSchema = SchemaFactory.createForClass(SocketModel);
