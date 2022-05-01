44bac240f6edd5c78e32547d0981f3ae34890d59
Kwon <anstjdrnjs3@gmail.com>
2022-05-01 23:28:03 +0900

    socket

--- NestJS/secondProject/public/js/scripts.js
+++ NestJS/secondProject/public/js/scripts.js
@@ -7,23 +7,47 @@ const chattingBox = getElementById('chatting_box');
 const formElement = getElementById('chat_form');
 
 socket.on('connected_user', (username) => {
-  console.log(username);
+  drawNewChat(`${username} connected!`);
 });
+
+socket.on('new_chat', (data) => {
+  const { context, username } = data;
+  drawNewChat(`${username}: ${context}`);
+});
+
+function handleSubmit(e) {
+  e.preventDefault();
+  context = e.target.elements[0].value;
+  e.target.elements[0].value = '';
+
+  if (context !== '') {
+    socket.emit('chat', context);
+    drawNewChat(`나: ${context}`);
+  }
+}
 
 function helloUser() {
   const userName = prompt('What is your name?');
 
-  const drawHelloStranger = (username) => {
-    header.innerText = `Hello ${username} Stranger`;
-  };
   // 리턴값으로 응답 받기
   socket.emit('new_user', userName, (username) => {
     drawHelloStranger(username);
   });
 }
+function drawNewChat(message) {
+  const wrapperChatBox = document.createElement('div');
+  const chatBox = `<div>${message}</div>`;
 
+  wrapperChatBox.innerHTML = chatBox;
+  chattingBox.append(wrapperChatBox);
+}
+
+const drawHelloStranger = (username) => {
+  header.innerText = `Hello ${username} Stranger`;
+};
 function init() {
   helloUser();
+  formElement.addEventListener('submit', handleSubmit);
 }
 
 init();
