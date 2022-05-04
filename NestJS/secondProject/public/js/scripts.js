3c72da81b64455741e5112f8e48b71c1f32e969a
Kwon <anstjdrnjs3@gmail.com>
2022-05-04 17:07:52 +0900

    modify js

--- NestJS/secondProject/public/js/scripts.js
+++ NestJS/secondProject/public/js/scripts.js
@@ -2,51 +2,66 @@ const socket = io('/chattings');
 
 const getElementById = (id) => document.getElementById(id) || null;
 
-const header = getElementById('hello_stranger');
-const chattingBox = getElementById('chatting_box');
+//* get DOM element
+const helloStrangerElement = getElementById('hello_stranger');
+const chattingBoxElement = getElementById('chatting_box');
 const formElement = getElementById('chat_form');
 
-socket.on('connected_user', (username) => {
+//* global socket handler
+socket.on('user_connected', (username) => {
   drawNewChat(`${username} connected!`);
 });
-
 socket.on('new_chat', (data) => {
-  const { context, username } = data;
-  drawNewChat(`${username}: ${context}`);
+  const { chat, username } = data;
+  drawNewChat(`${username}: ${chat}`);
 });
+socket.on('disconnect_user', (username) => drawNewChat(`${username}: bye...`));
 
-function handleSubmit(e) {
-  e.preventDefault();
-  context = e.target.elements[0].value;
-  e.target.elements[0].value = '';
-
-  if (context !== '') {
-    socket.emit('chat', context);
-    drawNewChat(`나: ${context}`);
+//* event callback functions
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const inputValue = event.target.elements[0].value;
+  if (inputValue !== '') {
+    socket.emit('submit_chat', inputValue);
+    // 화면에다가 그리기
+    drawNewChat(`나 : ${inputValue}`, true);
+    event.target.elements[0].value = '';
   }
-}
+};
+
+//* draw functions
+const drawHelloStranger = (username) =>
+  (helloStrangerElement.innerText = `Hello ${username} Stranger :)`);
+const drawNewChat = (message, isMe = false) => {
+  const wrapperChatBox = document.createElement('div');
+  wrapperChatBox.className = 'clearfix';
+  let chatBox;
+  if (!isMe)
+    chatBox = `
+    <div class='bg-gray-300 w-3/4 mx-4 my-2 p-2 rounded-lg clearfix break-all'>
+      ${message}
+    </div>
+    `;
+  else
+    chatBox = `
+    <div class='bg-white w-3/4 ml-auto mr-4 my-2 p-2 rounded-lg clearfix break-all'>
+      ${message}
+    </div>
+    `;
+  wrapperChatBox.innerHTML = chatBox;
+  chattingBoxElement.append(wrapperChatBox);
+};
 
 function helloUser() {
-  const userName = prompt('What is your name?');
-
-  // 리턴값으로 응답 받기
-  socket.emit('new_user', userName, (username) => {
-    drawHelloStranger(username);
+  const username = prompt('What is your name?');
+  socket.emit('new_user', username, (data) => {
+    drawHelloStranger(data);
   });
 }
-function drawNewChat(message) {
-  const wrapperChatBox = document.createElement('div');
-  const chatBox = `<div>${message}</div>`;
 
-  wrapperChatBox.innerHTML = chatBox;
-  chattingBox.append(wrapperChatBox);
-}
-
-const drawHelloStranger = (username) => {
-  header.innerText = `Hello ${username} Stranger`;
-};
 function init() {
   helloUser();
+  // 이벤트 연결
   formElement.addEventListener('submit', handleSubmit);
 }
 
